a4ab891713fd3010d2235e7396f4f35a71653ca5
m-lm <31556425+m-lm@users.noreply.github.com>
2025-02-16 23:40:50 -0600

    setup dataframes and SVD base

---
+++ src/svd.py
@@ -0,0 +1,9 @@
+from surprise import Dataset, Reader, SVD
+from surprise.model_selection import cross_validate
+import eda
+
+reader = Reader(rating_scale=(0, 10))
+data = Dataset.load_from_df(eda.ratings, reader)
+
+model = SVD()
+cross_validate(model, data, cv=5, verbose=True)
